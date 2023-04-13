1fa2c8a63409f3b4dd95602b8828738b4b85c023
Ray <zhenxinfrozen@gmail.com>
2023-04-13 15:40:26 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>我的简单网站</title>
+</head>
+<body>
+  <h1>欢迎来到我的简单网站！</h1>
+  <p>这是一个简单的 GitHub Pages 网站。</p>
+</body>
+</html>
