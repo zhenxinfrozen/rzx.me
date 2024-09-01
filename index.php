1dbbf3dba0657e4ced97c1962095a0798abfe3aa
Ray <zhenxinfrozen@gmail.com>
2024-09-01 17:11:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>我的简单网站</title>
+  <title>Ray's Github</title>
 </head>
 <body>
   <h1>光阴似箭，转瞬即逝，若不把握当下，便只能怀念过去</h1>
