5a845dc9ab12b32c54a7bafc6f84f05ff87204a6
restorer <restore@example.com>
2025-09-21 03:52:48 +0800

    fix(html): render header in body start for semantic correctness

--- public/ray-comic.php
+++ public/ray-comic.php
@@ -10,13 +10,12 @@ $title = 'Comic';
     <meta charset="utf-8">
     <title><?php echo htmlspecialchars($title, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'); ?></title>
     <link href="<?php echo htmlspecialchars(ASSET_URL, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'); ?>css/comic.css" rel="stylesheet" type="text/css" />
-    <?php
-    // render header include which also injects header CSS
-    echo render_template(__DIR__ . '/../includes/views/header.php', ['title' => $title]);
-    ?>
 </head>
 <body>
     <?php
+    // render header at top of body (header template may inject its own CSS)
+    echo render_template(__DIR__ . '/../includes/views/header.php', ['title' => $title]);
+
     // main body
     echo render_template(__DIR__ . '/../includes/views/ray-comic-body.php');
 
