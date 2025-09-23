d1511776d0a827970beb14873329ad8129074a18
restorer <restore@example.com>
2025-09-23 17:19:12 +0800

    Refactor: move ray-about view to app/views and remove public/ray-about.php (backup created)

---
+++ app/views/ray-about-body.php
@@ -0,0 +1,26 @@
+<div id="ray-about-bg-wallpaper"><img src="assets/images/about-left-hand-try.jpg" alt="left-hand"></div>
+<div id="ray-about-wrapper">
+    <div id="ray-about-connectbox">
+        <div id="email"></div>
+        <div id="ray-about-center-follow-icons">
+            <a href="https://plus.google.com/105722500346157763322" target="_blank" >
+            <div id="follow-google" class="follow-icon"></div>
+            </a>
+            <a href="http://www.twitter.com/rzxme" target="_blank">
+            <div id="follow-twitter" class="follow-icon"></div>
+            </a>
+            <a href="http://weibo.com/zhenxinfrzen" target="_blank">
+            <div id="follow-weibo" class="follow-icon"></div>
+            </a>
+            <a href="http://wpa.qq.com/msgrd?v=3&uin=327456910&site=qq&menu=yes" target="_blank" >
+            <div id="follow-qq" class="follow-icon"></div>
+            </a>
+        </div>
+    <div id="ray-about-note">
+            闲,忙,无聊,<font color="#CC3300">受不了</font>....<br /><br />
+            Beta:2.3 _ 2011.07.15
+    </div>        
+    </div>
+    
+        
+</div>
