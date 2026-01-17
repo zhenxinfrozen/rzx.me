3927775b7a4e4eaba40f1746a093d4d50575224b
Ray <zhenxinfrozen@gmail.com>
2026-01-18 05:44:36 +0800

    伪静态开关
    ture,不需要伪静态

--- app/Config/app.php
+++ app/Config/app.php
@@ -10,7 +10,7 @@ return [
         'debug' => true,
         'timezone' => 'Asia/Shanghai',
         'charset' => 'UTF-8',
-        'use_pretty_urls' => true, // Set to true if you have URL rewriting (pseudo-static) enabled
+        'use_pretty_urls' => true, // Set to true if you have URL rewriting (pseudo-static) enabled 伪静态
     ],
 
     // 路径配置
