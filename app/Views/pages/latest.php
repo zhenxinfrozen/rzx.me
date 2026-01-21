44893b3aaa3b80378ba5f2e3986b884f420ae404
restorer <restore@example.com>
2026-01-21 21:47:01 +0800

    movie-videos

--- app/Views/pages/latest.php
+++ app/Views/pages/latest.php
@@ -3,11 +3,11 @@
     <div align="center">
         <div align="center">
             <div class="shipin00">
-                <video poster="/assets/movie/nervous640.jpg" id="movie" preload controls width="640" height="360">
+                <video poster="/assets/videos/nervous640.jpg" id="movie" preload controls width="640" height="360">
                     <source src="#" type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"' />
                     <source src="#" type='video/webm; codecs="vp8.0, vorbis"' />
                     <div class="html5-tip">
-                        <p><img src="/assets/movie/html5-150.png" alt="HTML5" /></p>
+                        <p><img src="/assets/videos/html5-150.png" alt="HTML5" /></p>
                         <p>本页面视频采用HTML5标准</p>
                         <p>请使用<a href="http://www.google.cn/chrome/intl/zh-CN/landing_chrome.html?hl=zh" title="谷歌浏览器,本人推荐!!">Chrome</a>,
                            <a href="http://firefox.com.cn/" title="火狐浏览器">Firefox</a>, 或者
